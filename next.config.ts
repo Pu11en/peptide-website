a16c213aacf447be07b3cd60dd386a6e8c6901fa
Drew Pullen <YOUR_GITHUB_EMAIL@example.com>
2025-09-24 08:18:59 -0500

    Force HTTPS with upgrade-insecure-requests directive

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-ancestors 'none';"
+            value: "upgrade-insecure-requests; default-src https: 'self'; script-src https: 'self' 'unsafe-inline' 'unsafe-eval'; style-src https: 'self' 'unsafe-inline'; img-src https: 'self' data: blob:; font-src https: 'self'; connect-src https: 'self'; media-src https: 'self'; object-src 'none'; frame-ancestors 'none';"
           }
         ]
       }
@@ -58,6 +58,9 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+  // Force HTTPS
+  poweredByHeader: false,
+  compress: true,
 }
 
 export default nextConfig
